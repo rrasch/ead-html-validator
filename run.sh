ccefd93d174c50d37ec4af22e572400e5374f6cb
Rasan Rasch <rasan@nyu.edu>
2022-10-27 18:48:30 -0400

    Use html from github

--- run.sh
+++ run.sh
@@ -2,5 +2,7 @@
 
 export PYTHONUNBUFFERED=true
 
-./ead-html-validator.py -d Omega-EAD.xml ~/omega/guides/tamwag/mos_2021/ 2>&1 | tee out.log
+#./ead-html-validator.py -d Omega-EAD.xml ~/omega/guides/tamwag/mos_2021/ 2>&1 | tee out.log
+
+./ead-html-validator.py -d Omega-EAD.xml ~/work/findingaids-hugo-public/tamwag/mos_2021 2>&1 | tee out.log
 
