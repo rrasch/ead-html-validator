68fc16ba7bb302d7fbcc9c70af8f71ceb0b61584
Rasan Rasch <rasan@nyu.edu>
2023-03-15 23:25:05 -0400

    Add support for memory-profiler

--- runtimes.sh
+++ runtimes.sh
@@ -8,7 +8,11 @@ HTML_ROOT=$HOME/work/scratch/with-do-lookup/public
 
 CMD="$HOME/work/ead-html-validator/ead-html-validator.py"
 
-CSV_FILE="validator_runtimes_$(date +'%Y-%m-%d').csv"
+MPROF="mprof run --include-children"
+
+NOW=$(date +'%Y-%m-%d')
+
+CSV_FILE="validator_runtimes_${NOW}.csv"
 
 readarray -d '' EAD_FILES < <(find "$EAD_DIR" -name '*.xml' -print0 \
 	| egrep -vz '(no-ns|pretty).xml$')
@@ -36,9 +40,12 @@ do
 	echo -n "$partner,$collection" >> $CSV_FILE
 	for i in "${!ARGS[@]}"
 	do
+		desc=${ARGS[$i]#--}
+		desc=${desc:-single}
+		data_file="mprofile_${partner}_${collection}_${desc}_${NOW}.dat"
 		start_time=$(date +%s)
 		set +e
-		$CMD ${ARGS[$i]} "$file" "$html_dir"
+		$MPROF -o $data_file $CMD ${ARGS[$i]} "$file" "$html_dir"
 		set -e
 		end_time=$(date +%s)
 		duration=$((end_time - start_time))
