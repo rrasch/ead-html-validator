722afee903a67823681d7df9d9e364b317a00782
Rasan Rasch <rasan@nyu.edu>
2023-10-16 17:55:50 -0400

    Add type hints to eadhtml methods

--- ead_html_validator/eadhtml.py
+++ ead_html_validator/eadhtml.py
@@ -5,6 +5,7 @@ from lxml import etree as ET
 from pprint import pformat, pprint
 from string import punctuation
 from subprocess import PIPE
+from typing import List, Tuple
 import ead_html_validator.util as util
 import logging
 import re
@@ -26,41 +27,41 @@ class EADHTML:
         self.html_file = html_file
         self.main = self._main()
 
-    def author(self):
+    def author(self) -> ResultSet:
         return self.get_group_div_text("author")
 
-    def abstract(self):
+    def abstract(self) -> ResultSet:
         return self.formatted_note("abstract")
 
-    def accessrestrict(self):
+    def accessrestrict(self) -> ResultSet:
         return self.formatted_note("accessrestrict")
 
-    def accruals(self):
+    def accruals(self) -> ResultSet:
         return self.formatted_note("accruals")
 
-    def acqinfo(self):
+    def acqinfo(self) -> ResultSet:
         return self.formatted_note("acqinfo")
 
-    def altformavail(self):
+    def altformavail(self) -> ResultSet:
         return self.formatted_note("altformavail")
 
-    def appraisal(self):
+    def appraisal(self) -> ResultSet:
         return self.formatted_note("appraisal")
 
-    def arrangement(self):
+    def arrangement(self) -> ResultSet:
         return self.formatted_note("arrangement")
 
-    def bibliography(self):
+    def bibliography(self) -> ResultSet:
         return self.formatted_note("bibliography")
 
-    def bioghist(self):
+    def bioghist(self) -> ResultSet:
         return self.formatted_note_long("bioghist")
 
-    def c_count(self):
+    def c_count(self) -> int:
         return len(self.soup.find_all(class_=re.compile(r"^data-level")))
 
     # XXX recursive?
-    def chronlist(self):
+    def chronlist(self) -> ResultSet:
         items = ResultSet(value_type=dict)
         for clist in self.main.find_all(class_="ead-chronlist"):
             for item in clist.find_all(
@@ -74,7 +75,7 @@ class EADHTML:
                 items.add(item.name, {date: events}, item.sourceline)
         return items.rs_or_none()
 
-    def chronlist_heading(self):
+    def chronlist_heading(self) -> ResultSet:
         headings = ResultSet()
         for clist in self.main.find_all(class_="ead-chronlist"):
             values = self.find_all(class_=re.compile("head"), root=clist)
@@ -82,13 +83,17 @@ class EADHTML:
                 headings.append(values)
         return headings.rs_or_none()
 
-    def class_values(self, class_regex, all_values=False, get_text=False):
+    def class_values(
+        self, class_regex, all_values=False, get_text=False
+    ) -> ResultSet:
         # return self.find_all(class_=re.compile(class_regex), get_text=get_text)
         return self.class_values_helper(
             re.compile(class_regex), all_values=all_values, get_text=get_text
         )
 
-    def class_values_helper(self, class_expr, all_values=False, get_text=False):
+    def class_values_helper(
+        self, class_expr, all_values=False, get_text=False
+    ) -> ResultSet:
         result = ResultSet()
         for root in self.get_roots(all_values):
             values = self.find_all(
@@ -102,17 +107,17 @@ class EADHTML:
         return result.rs_or_none()
 
     @staticmethod
-    def clean_date(date):
+    def clean_date(date) -> str:
         punc = ",;"
         return util.clean_date(re.sub(r",\s*(bulk|inclusive)\s*$", "", date))
 
-    def collection(self):
+    def collection(self) -> ResultSet:
         return self.unittitle()
 
-    def collection_unitid(self):
+    def collection_unitid(self) -> ResultSet:
         return self.unitid()
 
-    def _component(self):
+    def _component(self) -> List[str]:
         components = self.soup.find("section", class_="c-items")
         ids = []
         for c in components.find_all(
@@ -121,7 +126,7 @@ class EADHTML:
             ids.append(c["id"])
         return ids
 
-    def component(self):
+    def component(self) -> List[CompHTML]:
         regex = re.compile(r"^level")
         first_c = self.soup.find("div", class_=regex)
         if first_c is None:
@@ -141,16 +146,16 @@ class EADHTML:
             logging.trace(f"({comp.level}, {comp.id})")
         return comps
 
-    def component_id_level(self):
+    def component_id_level(self) -> List[Tuple[str, str]]:
         return [(comp.id, comp.level) for comp in self.component()]
 
-    def contents(self):
+    def contents(self) -> List[str]:
         return [text for text in self.soup.stripped_strings]
 
-    def control_access(self):
+    def control_access(self) -> Tag:
         return self.main.find(class_="md-group controlaccess", recursive=False)
 
-    def control_access_group(self, field):
+    def control_access_group(self, field) -> ResultSet:
         ctrl_access = self.control_access()
         if not ctrl_access:
             return None
@@ -164,13 +169,13 @@ class EADHTML:
             )
         return result if result else None
 
-    def control_access_group_val(self, field, all_values=False):
+    def control_access_group_val(self, field, all_values=False) -> ResultSet:
         if all_values:
             return self.control_access_group_val_all(field)
         else:
             return self.control_access_group_val_top(field)
 
-    def control_access_group_val_all(self, field):
+    def control_access_group_val_all(self, field) -> ResultSet:
         ctrl_values = ResultSet()
         for group in self.soup.find_all(class_=f"controlaccess-{field}-group"):
             values = self.find_all(root=group, class_="controlaccess-value")
@@ -178,7 +183,7 @@ class EADHTML:
                 ctrl_values.append(values)
         return ctrl_values if ctrl_values else None
 
-    def control_access_group_val_top(self, field):
+    def control_access_group_val_top(self, field) -> ResultSet:
         ctrl_access = self.control_access()
         if not ctrl_access:
             return None
@@ -187,7 +192,7 @@ class EADHTML:
             return None
         return self.find_all(root=group, class_="controlaccess-value")
 
-    def corpname(self):
+    def corpname(self) -> ResultSet:
         corpnames = ResultSet()
         for result in [
             self.control_access_group_val("corpname"),
@@ -197,11 +202,11 @@ class EADHTML:
                 corpnames.append(result)
         return corpnames if corpnames else None
 
-    def creation_date(self):
+    def creation_date(self) -> ResultSet:
         creation = self.soup.find("div", class_="creation")
         return self.find_all(class_="ead-date", root=creation)
 
-    def creator(self):
+    def creator(self) -> ResultSet:
         origination = self.soup.find(
             class_=re.compile(r"^md-group origination")
         )
@@ -212,42 +217,44 @@ class EADHTML:
             get_text=False,
         )
 
-    def creators(self):
+    def creators(self) -> ResultSet:
         return self.creator()
 
-    def custodhist(self):
+    def custodhist(self) -> ResultSet:
         return self.formatted_note_long("custodhist")
 
-    def dimensions(self):
+    def dimensions(self) -> ResultSet:
         return self.formatted_note("dimensions")
 
-    def eadid(self):
+    def eadid(self) -> ResultSet:
         urls = self.url()
         if not urls:
             return None
         return urls.update_values(lambda url: url.rstrip("/").split("/")[-1])
 
-    def eadnum(self):
+    def eadnum(self) -> ResultSet:
         return self.find_all("span", class_="ead-num")
 
     @staticmethod
-    def is_parent_not_nav_link(tag):
+    def is_parent_not_nav_link(tag) -> bool:
         return tag.parent.get("class", "") != "nav-link"
 
-    def ead_class_values(self, class_name, all_values=False, get_text=True):
+    def ead_class_values(
+        self, class_name, all_values=False, get_text=True
+    ) -> ResultSet:
         return self.class_values_helper(
             re.compile(rf"^ead-{class_name}($| )"),
             all_values=all_values,
             get_text=get_text,
         )
 
-    def editionstmt(self):
+    def editionstmt(self) -> ResultSet:
         return self.formatted_note("editionstmt")
 
-    def extent(self):
+    def extent(self) -> ResultSet:
         return self.formatted_note("extent")
 
-    def famname(self):
+    def famname(self) -> ResultSet:
         famnames = ResultSet()
         for result in [
             self.find_all("div", class_=re.compile("^famname"), get_text=False),
@@ -257,7 +264,7 @@ class EADHTML:
                 famnames.append(result)
         return famnames if famnames else None
 
-    def fasb_version(self):
+    def fasb_version(self) -> str:
         return self.soup.find("meta", attrs={"name": "appversion"})["content"]
 
     def find_all(
@@ -273,7 +280,7 @@ class EADHTML:
         ignore_space=False,
         clean_txt=True,
         **kwargs,
-    ):
+    ) -> ResultSet:
         if root is None:
             root = self.soup
         nodes = root.find_all(*args, **kwargs)
@@ -309,7 +316,7 @@ class EADHTML:
 
         return result.rs_or_none()
 
-    def find_component(self, cid):
+    def find_component(self, cid) -> CompHTML:
         node = self.soup.find(attrs={"id": cid})
         if node is not None:
             if node.name == "div":
@@ -323,7 +330,7 @@ class EADHTML:
                 f"Can't find {cid} in {self.html_file}"
             )
 
-    def find_component_by_heading(self, head_id):
+    def find_component_by_heading(self, head_id) -> CompHTML:
         head = self.soup.find(re.compile(r"^h\d$"), {"id": head_id})
         if head is not None:
             return CompHTML(head.parent, head_id)
@@ -332,7 +339,9 @@ class EADHTML:
                 f"Can't find {head_id} in {self.html_file}"
             )
 
-    def formatted_note(self, field, join_text=False, join_sep="", **kwargs):
+    def formatted_note(
+        self, field, join_text=False, join_sep="", **kwargs
+    ) -> ResultSet:
         notes = self.main.find_all(
             "div", class_=f"md-group formattednote {field}", recursive=False
         )
@@ -348,7 +357,10 @@ class EADHTML:
             else:
                 search_root = note
             values = self.find_all(
-                re.compile("^(div|p)$"), root=search_root, recursive=False, **kwargs
+                re.compile("^(div|p)$"),
+                root=search_root,
+                recursive=False,
+                **kwargs,
             )
             if values:
                 result.append(values)
@@ -360,7 +372,7 @@ class EADHTML:
         else:
             return result
 
-    def formatted_note_long(self, *args, **kwargs):
+    def formatted_note_long(self, *args, **kwargs) -> ResultSet:
         notes = self.formatted_note(
             *args,
             sep=" ",
@@ -369,16 +381,16 @@ class EADHTML:
         )
         return notes.join(sep=" ") if notes else None
 
-    def function(self):
+    def function(self) -> ResultSet:
         return self.control_access_group("function")
 
-    def genreform(self):
+    def genreform(self) -> ResultSet:
         return self.get_field("genreform")
 
-    def geogname(self):
+    def geogname(self) -> ResultSet:
         return self.get_field("geogname")
 
-    def get_field(self, field, all_values=False):
+    def get_field(self, field, all_values=False) -> ResultSet:
         result = ResultSet()
         for value in [
             self.control_access_group_val(field, all_values=all_values),
@@ -389,14 +401,14 @@ class EADHTML:
         return result if result else None
 
     # XXX use recursive
-    def get_group_div_text(self, group_name, **kwargs):
+    def get_group_div_text(self, group_name, **kwargs) -> ResultSet:
         group = self.soup.find("div", class_=f"md-group {group_name}")
         if group and group.div:
             return self.find_all("div", root=group, **kwargs)
         else:
             return None
 
-    def get_roots(self, all_values):
+    def get_roots(self, all_values) -> List[Tag]:
         if all_values:
             roots = [self.main]
         else:
@@ -407,10 +419,10 @@ class EADHTML:
             ]
         return roots
 
-    def heading(self):
+    def heading(self) -> ResultSet:
         return self.unittitle()
 
-    def langcode(self):
+    def langcode(self) -> ResultSet:
         lang_codes = ResultSet()
         for lang_result in self.language():
             pyc_lang = pycountry.languages.get(name=lang_result["value"])
@@ -420,10 +432,10 @@ class EADHTML:
                 )
         return lang_codes if lang_codes else None
 
-    def language(self):
+    def language(self) -> ResultSet:
         return self.find_all(class_="ead-language")
 
-    def langusage(self):
+    def langusage(self) -> ResultSet:
         lang_usage = self.find_all(class_="langusage")
         if lang_usage:
             return lang_usage.update_values(
@@ -432,16 +444,16 @@ class EADHTML:
         else:
             return None
 
-    def _main(self):
+    def _main(self) -> Tag:
         return self.soup.find("main")
 
-    def materialspec(self):
+    def materialspec(self) -> ResultSet:
         return self.formatted_note("materialspec")
 
-    def material_type(self):
+    def material_type(self) -> ResultSet:
         return self.get_field("genreform", all_values=True)
 
-    def md_group(self, group):
+    def md_group(self, group) -> ResultSet:
         groups = self.main.find_all(class_=f"md-group {group}", recursive=False)
         if not groups:
             return None
@@ -452,10 +464,10 @@ class EADHTML:
                 text.append(result)
         return text if text else None
 
-    def name(self):
+    def name(self) -> ResultSet:
         return self.find_all(class_="ead-name")
 
-    def names(self):
+    def names(self) -> ResultSet:
         all_names = ResultSet()
 
         corp_regex = r"corpname( |$)"
@@ -480,26 +492,26 @@ class EADHTML:
         for name_type in ["famname", "persname"]:
             for names in [
                 self.class_values(rf"{name_type}( |$)", all_values=True),
-                self.control_access_group_val_all(name_type)
+                self.control_access_group_val_all(name_type),
             ]:
                 if names:
                     all_names.append(names)
 
         return all_names if all_names else None
 
-    def note(self):
+    def note(self) -> ResultSet:
         return self.formatted_note("notestmt")
 
-    def occupation(self):
+    def occupation(self) -> ResultSet:
         return self.get_field("occupation")
 
-    def originalsloc(self):
+    def originalsloc(self) -> ResultSet:
         return self.formatted_note("originalsloc")
 
-    def odd(self):
+    def odd(self) -> ResultSet:
         return self.formatted_note("odd")
 
-    def persname(self):
+    def persname(self) -> ResultSet:
         all_persnames = ResultSet()
         pers_regex = r"persname( |$)"
         for persnames in [
@@ -510,25 +522,25 @@ class EADHTML:
                 all_persnames.append(persnames)
         return all_persnames if all_persnames else None
 
-    def physfacet(self):
+    def physfacet(self) -> ResultSet:
         return self.formatted_note("physfacet")
 
-    def phystech(self):
+    def phystech(self) -> ResultSet:
         return self.formatted_note("phystech")
 
-    def place(self):
+    def place(self) -> ResultSet:
         return self.get_field("geogname", all_values=True)
 
-    def processinfo(self):
+    def processinfo(self) -> ResultSet:
         return self.formatted_note("processinfo")
 
-    def prefercite(self):
+    def prefercite(self) -> ResultSet:
         return self.formatted_note("prefercite")
 
-    def relatedmaterial(self):
+    def relatedmaterial(self) -> ResultSet:
         return self.formatted_note("relatedmaterial")
 
-    def repository(self):
+    def repository(self) -> ResultSet:
         repo = self.soup.find("div", class_="md-group repository")
         if not repo:
             return None
@@ -536,38 +548,38 @@ class EADHTML:
         # return EADHTML.resultset(repo.div) if repo.div else None
 
     @staticmethod
-    def resultset(node, xpath=None):
+    def resultset(node, xpath=None) -> ResultSet:
         if not node:
             raise ValueError("Must give a value for node.")
         return ResultSet(xpath=xpath).add(
             node.name, node.get_text(), node.sourceline
         )
 
-    def revisiondesc(self):
+    def revisiondesc(self) -> ResultSet:
         return self.formatted_note("revisiondesc")
 
-    def root(self):
+    def root(self) -> None:
         pass
 
-    def scopecontent(self):
+    def scopecontent(self) -> ResultSet:
         return self.formatted_note_long("scopecontent")
 
-    def separatedmaterial(self):
+    def separatedmaterial(self) -> ResultSet:
         return self.formatted_note("separatedmaterial")
 
-    def series(self):
+    def series(self) -> None:
         pass
 
-    def _sponsor(self):
+    def _sponsor(self) -> ResultSet:
         return self.md_group("sponsor")
 
-    def sponsor(self):
+    def sponsor(self) -> ResultSet:
         return self.xpath("//main/div[@class='md-group sponsor']/div")
 
-    def subject(self):
+    def subject(self) -> ResultSet:
         return self.get_field("subject")
 
-    def subjects(self):
+    def subjects(self) -> ResultSet:
         subj_set = ResultSet()
         for field in ["subject", "function", "occupation"]:
             for subjs in [
@@ -578,10 +590,10 @@ class EADHTML:
                     subj_set.append(subjs)
         return subj_set if subj_set else None
 
-    def subtitle(self):
+    def subtitle(self) -> ResultSet:
         return self.find_all(re.compile(r"^h\d$"), class_="subtitle")
 
-    def title(self):
+    def title(self) -> ResultSet:
         title_set = ResultSet()
         for titles in [
             self.get_field("title"),
@@ -591,13 +603,13 @@ class EADHTML:
                 title_set.append(titles)
         return title_set.rs_or_none()
 
-    def title_head(self):
+    def title_head(self) -> ResultSet:
         return EADHTML.resultset(self.soup.title)
 
-    def unitdate(self):
+    def unitdate(self) -> ResultSet:
         return self.get_group_div_text("unit_date", clean_txt=False)
 
-    def unitdate_all(self):
+    def unitdate_all(self) -> ResultSet:
         dates = self.unitdate()
         if dates:
             dates = dates.update_values(util.clean_date)
@@ -606,32 +618,32 @@ class EADHTML:
         else:
             return None
 
-    def unitdate_bulk(self):
+    def unitdate_bulk(self) -> ResultSet:
         return self.unitdate().grep(lambda date: "bulk" in date)
 
-    def unitdate_inclusive(self):
+    def unitdate_inclusive(self) -> ResultSet:
         return self.unitdate().grep(lambda date: "inclusive" in date)
 
-    def unitdate_not_type(self):
+    def unitdate_not_type(self) -> ResultSet:
         return self.unitdate().grep(
             lambda date: all(
                 dtype not in date for dtype in ["bulk", "inclusive"]
             )
         )
 
-    def unittitle(self):
+    def unittitle(self) -> ResultSet:
         return self.find_all(
             root=self.main, class_="page-title", recursive=False
         )
 
-    def unitid(self):
+    def unitid(self) -> ResultSet:
         return self.get_group_div_text("unit_id")
 
-    def url(self):
+    def url(self) -> ResultSet:
         return self.find_all("link", rel="canonical", attrib="href")
 
-    def userestrict(self):
+    def userestrict(self) -> ResultSet:
         return self.formatted_note("userestrict")
 
-    def xpath(self, xpath_expr):
+    def xpath(self, xpath_expr) -> ResultSet:
         return util.xpath(self.dom, xpath_expr, all_text=True)
